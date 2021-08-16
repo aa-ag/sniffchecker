d6ea77cf2bf2180972532cb933d853a0e7c4194a
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-15 19:28:15 -0500

    added function sniff and process_sniffed_packet to try to sniff eth0

--- check.py
+++ check.py
@@ -2,12 +2,15 @@
 import scapy.all as scapy
 
 
-############------------ GLOBAL VARIABLE(S) ------------############
-
-
 ############------------ FUNCTION(S) ------------############
+def sniff(what_to_sniff):
+    scapy.sniff(iface=what_to_sniff, store=False, prn=process_sniffed_packet)
 
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+        print(packet.show())
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    sniff("eth0")
